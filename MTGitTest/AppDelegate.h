4e258f6fcbb2d1ac0af7d1b52eeee7ad67f98c52
bianguangsheng <bianguangsheng@github.com>
2017-03-06 16:51:47 +0800

    Initial Commit

---
+++ MTGitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MTGitTest
+//
+//  Created by bianguangsheng on 2017/3/6.
+//  Copyright © 2017年 soufunnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MTGitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MTGitTest
+//
+//  Created by bianguangsheng on 2017/3/6.
+//  Copyright © 2017年 soufunnet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
