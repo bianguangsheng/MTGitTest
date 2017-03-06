ca6158411cb82608ba3ed4fe5aa59879956e60d2
bianguangsheng <bianguangsheng@github.com>
2017-03-06 17:15:58 +0800

    button 设置标题

--- MTGitTest/ViewController.m
+++ MTGitTest/ViewController.m
@@ -17,6 +17,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *btn=[[UIButton alloc]init];
+    [btn setTitle:@"button" forState:UIControlStateNormal];
+    
 }
 
 
